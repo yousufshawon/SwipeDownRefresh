b4eb6cd5b4d125aa34df4993ef01a1a03c5c3a5c
Abu Yousuf <yousuf.kuet08@gmail.com>
2016-04-24 11:57:50 +0600

    Initial commit

---
+++ app/src/main/java/com/yousuf/shawon/swipedownrefresh/activity/MainActivity.java
@@ -0,0 +1,157 @@
+package com.yousuf.shawon.swipedownrefresh.activity;
+
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.yousuf.shawon.swipedownrefresh.R;
+import com.yousuf.shawon.swipedownrefresh.app.MyApplication;
+import com.yousuf.shawon.swipedownrefresh.helper.Movie;
+import com.yousuf.shawon.swipedownrefresh.helper.SwipeListAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
+
+    private String TAG = MainActivity.class.getSimpleName();
+
+    private String URL_TOP_250 = "http://api.androidhive.info/json/imdb_top_250.php?offset=";
+
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private ListView listView;
+    private SwipeListAdapter adapter;
+    private List<Movie> movieList;
+
+    // initially offset will be 0, later will be updated while parsing the json
+    private int offSet = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        listView = (ListView) findViewById(R.id.listView);
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
+
+        movieList = new ArrayList<>();
+        adapter = new SwipeListAdapter(this, movieList);
+        listView.setAdapter(adapter);
+
+        swipeRefreshLayout.setOnRefreshListener(this);
+
+        /**
+         * Showing Swipe Refresh animation on activity create
+         * As animation won't start on onCreate, post runnable is used
+         */
+        swipeRefreshLayout.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        swipeRefreshLayout.setRefreshing(true);
+
+                                        fetchMovies();
+                                    }
+                                }
+        );
+
+
+
+    }
+
+
+
+
+    /**
+     * This method is called when swipe refresh is pulled down
+     */
+    @Override
+    public void onRefresh() {
+        Log.d(TAG, "onRefresh");
+        fetchMovies();
+    }
+
+
+
+
+    /**
+     * Fetching movies json by making http call
+     */
+    private void fetchMovies() {
+
+        // showing refresh animation before making http call
+        swipeRefreshLayout.setRefreshing(true);
+
+        // appending offset to url
+        String url = URL_TOP_250 + offSet;
+
+        // Volley's json array request object
+        JsonArrayRequest req = new JsonArrayRequest(url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d(TAG, response.toString());
+
+                        if (response.length() > 0) {
+
+                            // looping through json and adding to movies list
+                            for (int i = 0; i < response.length(); i++) {
+                                try {
+                                    JSONObject movieObj = response.getJSONObject(i);
+
+                                    int rank = movieObj.getInt("rank");
+                                    String title = movieObj.getString("title");
+
+                                    Movie m = new Movie(rank, title);
+
+                                    movieList.add(0, m);
+
+                                    // updating offset value to highest value
+                                    if (rank >= offSet)
+                                        offSet = rank;
+
+                                } catch (JSONException e) {
+                                    Log.e(TAG, "JSON Parsing error: " + e.getMessage());
+                                }
+                            }
+
+                            adapter.notifyDataSetChanged();
+                        }
+
+                        // stopping swipe refresh
+                        swipeRefreshLayout.setRefreshing(false);
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Server Error: " + error.getMessage());
+
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+
+                // stopping swipe refresh
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
+        // Adding request to request queue
+        MyApplication.getInstance().addToRequestQueue(req);
+    }
+
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/yousuf/shawon/swipedownrefresh/helper/Movie.java
@@ -0,0 +1,19 @@
+package com.yousuf.shawon.swipedownrefresh.helper;
+
+/**
+ * Created by Yousuf on 4/24/2016.
+ */
+public class Movie {
+
+    public int id;
+    public String title;
+
+    public Movie() {
+    }
+
+    public Movie(int id, String title) {
+        this.title = title;
+        this.id = id;
+    }
+
+}
